47e96beab3c4e894386b683c0348c77558245a34
BenjaminAlvarezDiaz <benja77744@gmail.com>
2024-08-05 19:59:39 -0300

    Intentando hacer un appbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
+
 
 function Home(){
     function onClick(){
@@ -8,10 +14,20 @@ function Home(){
     }
 
     return (
-    <button onClick= {onClick}>
-        Hazme click
-    </button>
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu">
+          <MenuIcon onClick= {onClick}/>
+        </IconButton>
+        <Typography variant="h6">
+          Mi App
+        </Typography>
+      </Toolbar>
+    </AppBar>
     );
+    /*<button onClick= {onClick}>
+        Hazme click
+    </button>*/
 };
 
 export default Home;
