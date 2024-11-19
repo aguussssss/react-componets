34ebcca4a30c3776aa222895d2eb0fe98ddd31b9
aguussssss <agustinquintanilla2006@gmail.com>
2024-11-18 22:57:04 -0300

    Añadir ruta a profile.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
-import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/Home.jsx";
-
+import React, { useState } from 'react';
+import Home from "./pages/Home.jsx";
+import Developer from "./pages/Developer.jsx";
+import ControlPanel from "./pages/ControlPanel.jsx";
+import AppBar from "./components/AppBar/AppBar.jsx";
+import Sidebar from "./components/SideBar/Sidebar.jsx";
+import TabBar from "./components/TabBar/TabBar.jsx";
+import "./App.css";
+import SearchBox from "./components/SearchBox/SearchBox.jsx";
+import Login from "./pages/Login.jsx";
+import Profile from "./pages/Profile.jsx";
 
 function App(){
     return (
         <div className="App">
             <Routes>
-                <Route path= "/" element= { <Home/>} />
-                <Route path= "/"/>
-                <Route path= "/"/>
+                <Route path= "/" element= {<Home/>} />
+                <Route path= "/developer" element= {<Developer/>} />
+                <Route path= "/control-panel" element= {<ControlPanel/>}/>
+                <Route path= "/login" element= {<Login/>}/>
+                <Route path= "/profile" element= {<Profile/>}/>
             </Routes>
         </div>
+        /*<div className="App">
+            <Routes>
+                <Route path= "/" element= {<Home/>}/>
+            </Routes>
+        </div>*/
     );
 }
 export default App;
